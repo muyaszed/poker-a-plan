77593719ef7ddbca70574ffac5e51d8949523321
Yazed Jamal <yazedjamal@Yazeds-MacBook-Air.local>
2021-12-12 09:55:16 +0000

    Added server with socket io

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const express = require("express");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log("Server is now connected to " + PORT))
